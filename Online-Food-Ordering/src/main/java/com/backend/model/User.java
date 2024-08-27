30cdcc4c54f633f419dd8a583b33f35b1679215d
emendo <n3.edson@hotmail.com>
2024-08-27 18:16:34 -0500

    Avance al 1:04 min - Entidad OrderItem culminado

--- Online-Food-Ordering/src/main/java/com/backend/model/User.java
+++ Online-Food-Ordering/src/main/java/com/backend/model/User.java
@@ -1,14 +1,15 @@
 package com.backend.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.backend.dto.RestaurantDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,8 +21,22 @@ public class User {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    private String fullName;
 
+    private String email;
 
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto>favorites = new ArrayList();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
 
 }
