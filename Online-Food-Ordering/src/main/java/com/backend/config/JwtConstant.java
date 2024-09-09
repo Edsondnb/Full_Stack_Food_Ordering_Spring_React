e3ab7d1674d1abd5c457d4ef308dff72824750ad
EDS <n3.edson@hotmail.com>
2024-09-09 14:41:46 -0500

    avance al 2:16 min

---
+++ Online-Food-Ordering/src/main/java/com/backend/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.backend.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="ioudjiaosudjiosajudkjncnxncoiopqwieoqwrweyriuweyruhfjdhskjjkncsndcsdnckjsnhjkhashsad";
+    public static final String JWT_HEADER="Authorization";
+
+}
