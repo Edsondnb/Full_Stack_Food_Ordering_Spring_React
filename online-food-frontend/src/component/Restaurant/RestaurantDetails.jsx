31856d45066aabee33cb692418caf50b6a1280fc
EDS <n3.edson@hotmail.com>
2024-10-09 14:51:50 -0500

    Avance F al 1:30:13

--- online-food-frontend/src/component/Restaurant/RestaurantDetails.jsx
+++ online-food-frontend/src/component/Restaurant/RestaurantDetails.jsx
@@ -1,6 +1,7 @@
-import { Grid } from '@mui/material'
+import { Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 const RestaurantDetails = () => {
   return (
@@ -28,12 +29,40 @@ const RestaurantDetails = () => {
             </div>
             <div className="pt-3 pb-5">
                 <h1 className='text-4xl font-semibold'>Indian Fast Food</h1>
-                <p className='text-gray-500 flex items-center gap-3'>
-                    <LocationOnIcon/>
-                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae maxime maiores natus ut dicta impedit, reprehenderit laboriosam quia suscipit repudiandae, explicabo delectus in quas. Officia animi eligendi consequuntur quo saepe.</span>
+                <p className='text-gray-500 mt-1'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet doloribus consequuntur rem dolorem, animi aliquam distinctio laboriosam dolores quas reprehenderit reiciendis a possimus placeat recusandae. Repellendus excepturi nobis id quis!
                 </p>
+
+                <div className="space-y-3 mt-3">
+                    <p className='text-gray-500 flex items-center gap-3'>
+                        <LocationOnIcon/>
+                        <span>Mumbai, Mahrstra</span>
+                    </p>
+                    <p className='text-gray-500 flex items-center gap-3'>
+                        <CalendarTodayIcon/>
+                        <span>Mon-sun: 9:00 AM - 9:00 PM (Today)</span>
+                    </p>
+                </div>
             </div>
         </section>
+
+        <Divider/>
+
+        <section className='pt-[2rem] lg:flex relative'>
+
+            <div className="space-y-10 lg:w-[20%] filter">
+                <div className="box space-y-5 lg:sticky top-28">
+                    <div className="">
+                        <Typography variant='h5' sx={{paddingBottom:"1rem"}}>
+                            Food 
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+            <div className="space-y-5 lg:w-[80%] lg:pl-10">menu</div>
+
+        </section>
+
+
 
     </div>
 
