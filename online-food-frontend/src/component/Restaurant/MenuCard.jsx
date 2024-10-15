3a92d50c130edb3a4f3aa9205cf602c85c45ff61
EDS <n3.edson@hotmail.com>
2024-10-15 13:39:30 -0500

    Avance F - 2:10:47

--- online-food-frontend/src/component/Restaurant/MenuCard.jsx
+++ online-food-frontend/src/component/Restaurant/MenuCard.jsx
@@ -1,14 +1,24 @@
-import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Button, Checkbox, FormControlLabel, FormGroup } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import React from 'react'
 
-const ingredients = [
+
+const demo = [
     { 
         category: "Nuts & Seeds",
+        ingredients: ["Cashews"]
+    },
+    {
+        category: "Protein",
+        ingredients: ["Ground Beef", "Bacon strips"]
     }
 ]
 
 export const MenuCard = () => {
+
+  const handleCheckBoxChange = (item) => {
+    console.log("value")
+  }
 
   return (
 
@@ -21,7 +31,7 @@ export const MenuCard = () => {
           <div className='lg:flex items-center justify-between'>
             <div className='lg:flex items-center lg:gap-5'>
                 <img className="w-[7rem] h-[7rem] object-cover"
-                src="" alt="" />
+                src="https://cdn.pixabay.com/photo/2016/08/31/21/47/burger-1634705_1280.jpg" alt="" />
                 <div className="space-y-1 lg:space-y-5 lg:max-w-2xl">
                     <p className='fonsemibold text-xl'>Burger</p>
                     <p>S/.49</p>
@@ -33,7 +43,25 @@ export const MenuCard = () => {
           </div>
         </AccordionSummary>
         <AccordionDetails>
-          
+          <form action="">
+            <div className="flex gap-5 flex-wrap">
+              {
+                demo.map((item) =>
+
+                  <div className="">
+                    <p>{item.category}</p>
+                    <FormGroup>
+                      {item.ingredients.map((item) => <FormControlLabel control={<Checkbox onChange={() => handleCheckBoxChange(item)}/>} label={item} />) }
+                      
+                    </FormGroup>
+                  </div>
+                )
+              }
+            </div>
+            <div className="pt-55">
+              <Button type='submit' variant='contained' disabled={false}>{true? "Add to cart" : "Out of stock"}</Button>
+            </div>
+          </form>
         </AccordionDetails>
       </Accordion>
 
