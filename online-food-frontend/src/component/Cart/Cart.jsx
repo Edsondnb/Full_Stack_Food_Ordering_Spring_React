30837980dcd8ef78f9e9c153e07ec6c0a9028d1b
EDS <n3.edson@hotmail.com>
2024-10-21 14:52:18 -0500

    Avance F al - 2:?

--- online-food-frontend/src/component/Cart/Cart.jsx
+++ online-food-frontend/src/component/Cart/Cart.jsx
@@ -1,15 +1,38 @@
-import { Divider } from '@mui/material'
+import { Box, Button, Card, Divider, Modal } from '@mui/material'
 import React from 'react'
 import { CartItem } from './CartItem'
+import { AddressCard } from './AddressCard'
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import { AddLocation } from '@mui/icons-material';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    outline: 'none',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 const items = [1,1,1]
 
 const Cart = () => {
+
+    const createOrderUsingSelectedAddress = () => {}
+
+    const handleOpenAddressModal = () => setOpen(true);
+
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => setOpen(false);
+
   return (
 
 
-    <div>
+    <>
         <main className='lg:flex justify-between'>
 
             <section className='lg:w-[30%] space-y-6 lg:min-h-screen pt-10'>
@@ -41,13 +64,38 @@ const Cart = () => {
                 </div>
             </section>
             <Divider orientation='vertical' flexItem/>
-            <section className='lg:w-[70%]'>
+            <section className='lg:w-[70%] flex justify-center px-5 pb-10 lg:pb-0'>
+                <div>
+                    <h1 className='text-center font-semibold text-2xl py-10'>Choose Delivery Address</h1>
+                    <div className="flex gap-5 flex-wrap justify-center">
+                        {[1,1,1,1,1].map((item) => ( 
+                            <AddressCard handleSelectAddress={createOrderUsingSelectedAddress} item={item} showButton={true}/>
+                        ))}
+                        <Card className='flex gap-5 w-64 p-5'>
+                            <AddLocation/>
+                            <div className="sapce-y-3 text-gray-500">
+                                <h1 className="font-semibold text-lg text-white">Add New Address</h1>
+                                <Button variant='outlined' onClick={handleOpenAddressModal}>Add</Button>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
 
             </section>
 
         </main>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx={style}>
+                
+            </Box>
+        </Modal>
 
-    </div>
+    </>
     
   )
 }
